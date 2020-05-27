58d26aa45aa3b9fae6b5d2e4bab63821fe58272b
Orawko <bartlomiej.orawiec@gmail.com>
2020-05-27 13:17:59 +0200

    Updates

---
+++ precache-manifest.33429b5c400ba194bc5dfcba5f52df52.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3002ca5f34e1da1cfbb945dc8df4782",
+    "url": "/edytamakowiec.github.io/index.html"
+  },
+  {
+    "revision": "c6adff40f82f36c14668",
+    "url": "/edytamakowiec.github.io/static/css/main.581edd56.chunk.css"
+  },
+  {
+    "revision": "f59d7fd3cce480816e11",
+    "url": "/edytamakowiec.github.io/static/js/2.ecac40cf.chunk.js"
+  },
+  {
+    "revision": "846e3710ca5ee3f911296f05447fe08c",
+    "url": "/edytamakowiec.github.io/static/js/2.ecac40cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6adff40f82f36c14668",
+    "url": "/edytamakowiec.github.io/static/js/main.a1f4cebd.chunk.js"
+  },
+  {
+    "revision": "4936888f7ccfc63752b0",
+    "url": "/edytamakowiec.github.io/static/js/runtime-main.c4effd1d.js"
+  },
+  {
+    "revision": "6057c965dba7bb51c58639dcd6afa8e1",
+    "url": "/edytamakowiec.github.io/static/media/1.6057c965.jpg"
+  },
+  {
+    "revision": "1a4077838e7eba3682606d4cdaa87222",
+    "url": "/edytamakowiec.github.io/static/media/2.1a407783.jpeg"
+  },
+  {
+    "revision": "9a2988facce23555a6e01861947ed615",
+    "url": "/edytamakowiec.github.io/static/media/aboutMePhoto.9a2988fa.png"
+  },
+  {
+    "revision": "0e3d1e14ddfd17d1aeb9aff4290c221a",
+    "url": "/edytamakowiec.github.io/static/media/fontello.0e3d1e14.ttf"
+  },
+  {
+    "revision": "3c48eb7860dee5a4f7b21ffa4e3de6aa",
+    "url": "/edytamakowiec.github.io/static/media/fontello.3c48eb78.woff2"
+  },
+  {
+    "revision": "53779ec30133581fd6b1a56a8ab00685",
+    "url": "/edytamakowiec.github.io/static/media/fontello.53779ec3.svg"
+  },
+  {
+    "revision": "86aefce5019fe16a346b92420d7e3985",
+    "url": "/edytamakowiec.github.io/static/media/fontello.86aefce5.eot"
+  },
+  {
+    "revision": "a96c4569f828cd6afc8b46cd75536d19",
+    "url": "/edytamakowiec.github.io/static/media/fontello.a96c4569.woff"
+  },
+  {
+    "revision": "6f3ded46929f5f4a40904f8d21bb71e0",
+    "url": "/edytamakowiec.github.io/static/media/header.6f3ded46.jpg"
+  },
+  {
+    "revision": "b51f121bfe3c80dfbbcfd7f78325f545",
+    "url": "/edytamakowiec.github.io/static/media/sports_centre.b51f121b.jpg"
+  }
+]);
