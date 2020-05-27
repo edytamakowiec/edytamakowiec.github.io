e74f15f719755951319376f00dfb79343542a277
Orawko <bartlomiej.orawiec@gmail.com>
2020-05-27 13:38:24 +0200

    static website

--- client/src/data/projectImgPaths.js
+++
@@ -1,8 +0,0 @@
-const paths = {
-  0: [{ src: require('../img/project1/1.jpg') }, { src: require('../img/project1/2.jpeg') }],
-  1: [{ src: require('../img/project1/2.jpeg') }, { src: require('../img/project1/1.jpg') }],
-  2: [{ src: require('../img/project1/1.jpg') }, { src: require('../img/project1/2.jpeg') }],
-  3: [{ src: require('../img/project1/2.jpeg') }, { src: require('../img/project1/1.jpg') }],
-};
-
-export default paths;
