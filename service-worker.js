528e9af0eb457996d7be785c83d42bc8c0ec447a
Orawko <bartlomiej.orawiec@gmail.com>
2020-05-30 18:50:07 +0200

    added final data

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/edytamakowiec.github.io/precache-manifest.33429b5c400ba194bc5dfcba5f52df52.js"
+  "/ArchitecturePortfolio/precache-manifest.3d4a86d680e541b68324197bb98c41d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/edytamakowiec.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ArchitecturePortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
